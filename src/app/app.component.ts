07990ae832c867064b4f0e558e1f35099f34b397
Eduardo Barros <barrosebs@gmail.com>
2019-07-05 13:31:32 -0300

    Criado a primeira versao do curso - Aula de 1 a 10 do Módulo 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+  /**
+   *
+   */
+  constructor() {
+    this.todos.push(new Todo(1,'Passear com o cachorro',false));
+    this.todos.push(new Todo(2,'Ir ao supermercado',false));
+    this.todos.push(new Todo(3,'Cortar o cabelo',true));
+  }
+
+  alterarTexto(){
+    this.title = 'Teste';
+  }
+}
---
+++ src/models/todo.model.ts
@@ -0,0 +1,14 @@
+//export defini que a class é public
+export class Todo{
+/**
+ *ctor
+ */
+constructor(
+    public id: Number,
+    public title: String,
+    public done: Boolean,
+ 
+) {
+    
+}
+}
