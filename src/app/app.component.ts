cff49ac061d291e3c4cc31e0bf33f6a91e194df1
Eduardo Barros <barrosebs@gmail.com>
2019-07-08 15:41:15 -0300

    aulas de 12 a 16 Adicionando uma tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,42 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
   /**
    *
    */
-  constructor() {
-    this.todos.push(new Todo(1,'Passear com o cachorro',false));
-    this.todos.push(new Todo(2,'Ir ao supermercado',false));
-    this.todos.push(new Todo(3,'Cortar o cabelo',true));
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    })
+
+  }
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, true))
+    this.clear();
   }
 
-  alterarTexto(){
-    this.title = 'Teste';
+  clear(){
+    this.form.reset();
+  }
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index,1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done=true
+  }
+
+  markAsUnDone(todo:Todo){
+    todo.done=false;
   }
 }
